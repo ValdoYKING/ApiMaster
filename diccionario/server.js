97db14b46826741b204de4e13e52eb9d0ce73c8d
valdoyk <osvaldovillalba-02@hotmail.com>
2025-02-22 02:02:01 -0600

    Mejora de la configuración de CORS para permitir orígenes dinámicos y métodos HTTP específicos

--- diccionario/server.js
+++ diccionario/server.js
@@ -7,7 +7,27 @@ import autenticacionRoutes from "./src/routes/autenticacion_routes.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+// Configuración de opciones de CORS
+const corsOptions = {
+    origin: function(origin, callback){
+        // Lista de orígenes permitidos
+        /* const allowedOrigins = ['http://localhost:3000', 'https://diccionario-cliente.herokuapp.com']; */
+        const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+        // Verifica si el origen de la solicitud está en la lista de orígenes permitidos
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+            // Si el origen está permitido, se llama al callback sin error
+            callback(null, true);
+        } else{
+            // Si el origen no está permitido, se llama al callback con un error
+            callback(new Error('No permitido por CORS'));
+        }
+    },
+    // Métodos HTTP permitidos
+    methods: "GET, POST, PUT, DELETE",
+}
+
+app.use(cors(corsOptions));
 
 connectDB();
 
