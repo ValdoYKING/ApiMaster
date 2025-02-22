1f67be0a00bdc29f98e6670406c7e98975e088a3
valdoyk <osvaldovillalba-02@hotmail.com>
2025-02-21 22:39:47 -0600

    Comentarios para una mejor comprension

--- diccionario/src/config/db.js
+++ diccionario/src/config/db.js
@@ -1,10 +1,14 @@
 // src/config/db.js
+// Función para conectar a MongoDB
 const connectDB = async () => {
     try {
+        // Intentar conectar a MongoDB usando la cadena de conexión de las variables de entorno
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB conecta")
+        console.log("MongoDB conectado");
     } catch (error) {
+        // Registrar un mensaje de error si la conexión falla
         console.log("Error al conectar a MongoDB");
+        // Salir del proceso con fallo
         process.exit(1);
     }
 };
